6713ddbb5d5141b874ef1984a287bf022c500345
LuckyLuckyLu <alu8@stuy.edu>
2018-02-11 22:25:02 -0500

    Finished lab. Has somethings I want to do at end of file.

--- 02BoredQueens/QueenBoard.java
+++ 02BoredQueens/QueenBoard.java
@@ -111,15 +111,15 @@ public class QueenBoard{
 
    */
   public boolean solve(){
-    boolean nonZeroPresence = false;
+    boolean nonZeroPresent = false;
     for (int i = 0; i < board.length; i++){
 	    for (int j = 0; j < board.length; j++){
         if (board[i][j] != 0){
-          nonZeroPresence = true;
+          nonZeroPresent = true;
         }
 	    }
     }
-    if (nonZeroPresence){
+    if (nonZeroPresent){
       throw new IllegalStateException();
     } else {
       return solHelper(0,0);
@@ -147,16 +147,82 @@ public class QueenBoard{
    *@return the number of solutions found, and leaves the board filled with only 0's
    *@throws IllegalStateException when the board starts with any non-zero value
    */
-  public int countSolutions(){return 0;}
-    
+  public int countSolutions(){
+    boolean nonZeroPresent = false;
+    for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board.length; j++){
+        if (board[i][j] != 0){
+          nonZeroPresent = true;
+        }
+	    }
+    }
+    if (nonZeroPresent){
+      throw new IllegalStateException();
+    } else {
+      return CountHelper(0,0);
+    }
+  }
+
+  private int CountHelper(int col, int qPlaced){
+    int solutions = 0;
+    if (col ==  board.length && board.length == qPlaced){
+      return 1;
+    } else if (col == board.length && board.length != qPlaced){
+      return 0;
+    }
+    for (int x = 0; x < board.length; x++){
+      if (addQueen(x,col)){
+        solutions += CountHelper(col+1, qPlaced+1);
+        rmQueen(x,col);
+      }
+    }
+    return solutions;
+  }
   public static void main(String[] args){
-    QueenBoard x = new QueenBoard(10);
+    QueenBoard x = new QueenBoard(4);
     //x.addQueen(5,8);
     System.out.println(x);
     //x.rmQueen(1,1);
     //System.out.println(x.addQueen(9,8));
     //System.out.println(x);
+    System.out.println(x.countSolutions());
+    System.out.println(x);
     x.solve();
     System.out.println(x);
+
+    
+    QueenBoard b = new QueenBoard(4);
+
+    System.out.println(b.solve()); //prints true
+    System.out.println(b); //prints a valid solution
+
+    try{
+      b.solve();
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    try{
+      b.countSolutions();
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    for (int i = 0; i < 12; i++){
+      QueenBoard a = new QueenBoard(i);
+      System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
+      /*          Expected Values
+       i --> # of Solutions   i --> # of Solutions
+      0 --> 1                      6 --> 4
+      1 --> 1                      7 --> 40
+      2 --> 0                      8 --> 92
+      3 --> 0                      9 --> 352
+      4 --> 2                    10 --> 724
+      5 --> 10                  11 --> 2680
+      */
+      System.out.println(a); //prints out an empty i by i grid of underscores
+    }
   }
 }
+// TODO: change the recursion through addition of parameters.
+// TODO: remove redundant code.
