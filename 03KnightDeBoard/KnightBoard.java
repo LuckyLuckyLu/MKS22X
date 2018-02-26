52de308c06cef9b8d3460bc4ed3ebe5cba17a47e
LuckyLuckyLu <alu8@stuy.edu>
2018-02-26 16:55:08 -0500

    Finally figured out how to fix methods. CS DOJO ROCKS! Also fixed duplicate in moveSet.

--- 03KnightDeBoard/KnightBoard.java
+++ 03KnightDeBoard/KnightBoard.java
@@ -1,104 +1,170 @@
 //import java.util.Arrays;
 public class KnightBoard{
-  private int[][] board;
-  public KnightBoard(int row, int col){
-    board = new int[row][col];
-    /*
-    //int x = 0;
-    for (int i = 0; i < row; i++){
-    for (int j = 0; j < col; j++){
-    board[i][j] = x;
-    //	x += 1;
+    private int[][] board;
+    public KnightBoard(int row, int col){
+	board = new int[row][col];
+	/*
+	//int x = 0;
+	for (int i = 0; i < row; i++){
+	for (int j = 0; j < col; j++){
+	board[i][j] = x;
+	//	x += 1;
+	}
+	}
+	*/
     }
-    }
-    */
-  }
 
-  public String toString(){
-    String result = "";
-    for (int i = 0; i < board.length; i++){
+    public String toString(){
+	String result = "";
+	for (int i = 0; i < board.length; i++){
 	    for (int j = 0; j < board[0].length; j++){
-        if (board[i][j] / 10 >= 1){
-          result += " " + board[i][j];
-        } else if (board[i][j] == 0){
-          result += " __";
-        } else {
-          result += " _" + board[i][j];
-        }
+		if (board[i][j] / 10 >= 1){
+		    result += " " + board[i][j];
+		} else if (board[i][j] == 0){
+		    result += " __";
+		} else {
+		    result += " _" + board[i][j];
+		}
 	    }
 	    result += "\n";
+	}
+	return result;
     }
-    return result;
-  }
-  private boolean moveKnight(int row, int col, int number){
-    if (row > board[0].length ||
-        row < 0 ||
-        col > board.length ||
-        col < 0 ||
-        board[row][col] != 0){
+    private boolean moveKnight(int row, int col, int number){
+	if (row >= board[0].length ||
+	    row < 0 ||
+	    col >= board.length ||
+	    col < 0 ||
+	    board[row][col] != 0){
 	    return false;
-    } else {
+	} else {
 	    board[row][col] = number;
 	    return true;
+	}
     }
-  }
-  private boolean rmKnight(int row, int col){
-    if (row > board[0].length ||
-        row < 0 ||
-        col > board.length ||
-        col < 0 ||
-        board[row][col] == 0){
-	    return false;
-    } else {
+    private void rmKnight(int row, int col){
+	if (row >= board[0].length ||
+	    row < 0 ||
+	    col >= board.length ||
+	    col < 0 ||
+	    board[row][col] != 0){
+	} else {
 	    board[row][col] = 0;
+	}
+    }    
+
+    public boolean solve(int row, int col){
+	return solveH(row,col,1);
+    }
+
+    private boolean solveH(int row, int col, int level){
+	int[][] moveSet = {{-1,-2},
+			   {-2,-1},
+			   {-1,2},
+			   {2,-1},
+			   {1,-2},
+			   {-2,1},
+			   {1,2},
+			   {2,1}};
+	
+	if (board[row][col] != 0){
+	    return false;
+	}
+	if (level == board.length * board[0].length){
+	    board[row][col] = level;
 	    return true;
-    }
-  }    
+	}
+	for (int x = 0; x < moveSet.length; x++){
+	    int newRow = row+ moveSet[x][0];
+	    int newCol = col+ moveSet[x][1];
+	    if (newRow >= board.length ||
+		newRow < 0 ||
+		newCol >= board[0].length ||
+		newCol < 0){
 
-  public boolean solve(int row, int col){
-    board[row][col] = 1;
-    return solveH(row,col,2);
-  }
-
-  private boolean solveH(int row, int col, int level){
-    int[][] moveSet = {{-1,-2},{-2,-1},{-1,2},{2,-1},{1,-2},{2,-1},{1,2},{2,1}};
-    for (int x = 0; x < moveSet.length; x++){
-	    if (moveKnight(row+moveSet[x][0],col+moveSet[x][1],level)){
-        //System.out.println(toString());
-        return solveH(row+moveSet[x][0],col+moveSet[x][1],level+1); 
 	    } else {
-        rmKnight(row+moveSet[x][0],col+moveSet[x][1]);	    
+		board[row][col] = level;
+		if (solveH(newRow,newCol,level+1)){
+		    return true;
+		}
+		board[row][col] = 0;
 	    }
-	    //System.out.println(toString());
+	}
+	return false;
     }
-    return false;
-  }
 
-  public int countSolutions(){
-    return 0;
-  }
+    public int countSolutions(int row, int col){
+	return countHelp(row,col,1);
+    }
 
-  private int countHelp(int counter){
-    return 0;
-  }
+    private int countHelp(int row, int col, int level){
+	int solutions = 0;
+	int[][] moveSet = {{-1,-2},
+			   {-2,-1},
+			   {-1,2},
+			   {2,-1},
+			   {1,-2},
+			   {-2,1},
+			   {1,2},
+			   {2,1}};
+	
+	if (board[row][col] != 0){
+	    //System.out.println(board[row][col]);
+	    return 0;
+	}
+	if (level == 25){//board.length * board[0].length){
+	    //board[row][col] = level;
+	    return 1;
+	}
+	for (int x = 0; x < moveSet.length; x++){
+	    //System.out.println("HI");
+	    int newRow = row+ moveSet[x][0];
+	    int newCol = col+ moveSet[x][1];
+	    if (newRow >= board.length ||
+		newRow < 0 ||
+		newCol >= board[0].length ||
+		newCol < 0){
+	    } else {
+		board[row][col] = level;
+		solutions += countHelp(newRow,newCol,level+1);
+		board[row][col] = 0;
+	    }
+	}
+	return solutions;
+    }
 
-  public static void main(String[] Args){
-    KnightBoard y = new KnightBoard(5,5);
-    /*System.out.println(y);
-      System.out.println(y.moveKnight(5,5,1));
-      System.out.println(y);
-      System.out.println(y.moveKnight(9,5,1));
-      System.out.println(y);
-      System.out.println(y.moveKnight(5,9,1));
-      System.out.println(y);
-      System.out.println(y.moveKnight(20,5,1));
-      System.out.println(y);
-      System.out.println(y.moveKnight(5,20,1));
-      System.out.println(y);*/
+    public static void main(String[] Args){
+	KnightBoard y = new KnightBoard(5,5);
+	/*System.out.println(y);
+	  System.out.println(y.moveKnight(5,5,1));
+	  System.out.println(y);
+	  System.out.println(y.moveKnight(9,5,1));
+	  System.out.println(y);
+	  System.out.println(y.moveKnight(5,9,1));
+	  System.out.println(y);
+	  System.out.println(y.moveKnight(20,5,1));
+	  System.out.println(y);
+	  System.out.println(y.moveKnight(5,20,1));
+	  System.out.println(y);*/
 
-    KnightBoard z = new KnightBoard(5,5);
-    System.out.println(z);
-    z.solve(0,0);
-    System.out.println(z);
-  }
+	KnightBoard z = new KnightBoard(5,5);
+	System.out.println(z);
+	System.out.println(z.solve(0,0));
+	System.out.println(z);
+	KnightBoard x = new KnightBoard(5,5);
+	System.out.println(x);
+	System.out.println(x.countSolutions(0,0));
+	System.out.println(x);
+			   
+    }
 }
+	    // if (moveKnight(row+moveSet[x][0],col+moveSet[x][1],level)){
+	    // 	//System.out.println(toString());
+	    // 	if (solveH(row+moveSet[x][0],col+moveSet[x][1],level+1)){
+	    // 	    return true;
+	    // 	}
+	    // 	board[row+moveSet[x][0]][col+moveSet[x][1]] = 0;	    
+		
+	    // }
+	    // board[row+moveSet[x][0]][col+moveSet[x][1]] = 0;	    
+	    // //System.out.println(toString());
