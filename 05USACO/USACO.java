a398501e6a0c7059ba08617bc0b09da6fef76808
LuckyLuckyLu <alu8@stuy.edu>
2018-03-05 15:46:27 -0500

    Working

---
+++ 05USACO/USACO.java
@@ -0,0 +1,15 @@
+public class USACO{
+    public static int bronze(String filename){
+	return 0;
+    }
+
+    public static int silver(String filename){
+	try (
+	     File file = new File(filename);
+	     Scanner in = new Scanner(file);
+	     while(in.hasNextLine()){
+		 
+	     }
+    }
+
+}
