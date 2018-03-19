3976422117842a11e5fc785df2fab8debaed87b9
LuckyLuckyLu <alu8@stuy.edu>
2018-03-19 15:24:30 -0400

    Worked on Quick.java

--- 06Stuff/Quick.java
+++ 06Stuff/Quick.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 public class Quick{
-    private int[] Array;    
-    public void Swap(int[] box, int x, int y){
+    //private int[] Array;    
+    public static void Swap(int[] box, int x, int y){
 	int ValueHolder  = box[x];
 	box[x] = box[y];
 	box[y] = ValueHolder;
@@ -15,59 +15,75 @@ public class Quick{
      *@return the index of the final position of the pivot element.
      */
 
-    public static int partition (int[] data, int start, int end){
+    public static int partition(int[] data, int start, int end){
 	Random index = new Random();
 	int v = index.nextInt(end) + start;
-	int i = start;
-	Swap(v,i);
-	i++;
+	System.out.println(v);
+	Swap(data,v,start);
+	int front = start + 1;
+	int ending = end;
+	System.out.println(toString(data));
 	//System.out.println(Array.length);
-	int j = end;
-	while(front <= end){
-	    if (Array[front] > Array[]){
-		Swap(front,end);
-		end --;
+	while(front <= ending){
+	    if (data[front] > data[ending]){
+		Swap(data,front,ending);
+		ending --;
 	    } else{
 		front ++;
 	    }
 	}
+	Swap(data,ending,start);
+	System.out.println(toString(data));
 
+	int i = 0;
+	for (int x = 0; x < data.length; x++){
+	    if (data[x] == v){
+		return i;
+	    }
+	    i++;
+	}
+	return i;
     }
     public static void quicksort(int[]ary){
 	
     }
-
-    public void Partition(int index){
-	int front = 0;
-	Swap(front,index);
-	int first = front;
-	front++;
-	System.out.println(Array.length);
-	int end = Array.length - 1;
-	System.out.println(end);
-	while(front <= end){
-	    if (Array[front] > Array[]){
-		Swap(front,end);
-		end --;
-	    } else{
-		front ++;
-	    }
-	}
-    }
-    public int[] getArray(){
-	return Array;
-    }
-    public String toString(){
+    public static String toString(int[] arr){
 	String result = "";
-	for (int x = 0; x < this.getArray().length; x++){
-	    result += this.getArray()[x] + " ";
+	for (int x = 0; x < arr.length; x++){
+	    result += arr[x];
 	}
 	return result;
     }
+    // public void Partition(int index){
+    // 	int front = 0;
+    // 	Swap(front,index);
+    // 	int first = front;
+    // 	front++;
+    // 	System.out.println(Array.length);
+    // 	int end = Array.length - 1;
+    // 	System.out.println(end);
+    // 	while(front <= end){
+    // 	    if (Array[front] > Array[]){
+    // 		Swap(front,end);
+    // 		end --;
+    // 	    } else{
+    // 		front ++;
+    // 	    }
+    // 	}
+    // }
+    // public int[] getArray(){
+    // 	return Array;
+    // }
+    // public String toString(){
+    // 	String result = "";
+    // 	for (int x = 0; x < this.getArray().length; x++){
+    // 	    result += this.getArray()[x] + " ";
+    // 	}
+    // 	return result;
+    // }
     public static void main(String[] Arg){
 	int[] x = {1,4,6,7,9,3,2};
-	QuickSelect bob = new QuickSelect(x);
-	bob.Partition();
-	System.out.println(bob);//tArray());//.toString());
+	Quick bob = new Quick();
+	System.out.println(bob.partition(x,0,6));//tArray());//.toString());
     }
 }
