a6cca8cb453fce8f60dc2ead05f1d23c82d9d862
LuckyLuckyLu <alu8@stuy.edu>
2018-03-20 16:57:12 -0400

    Merging in progress.

--- 07eMERGEncy/Merge.java
+++ 07eMERGEncy/Merge.java
@@ -1,17 +1,49 @@
 public class Merge{
-    public static void mergesort(int[] data){
+    // public static void mergesort(int[] data){
+    // 	if (data.length <= 1){
+	    
+    // 	} else {
+    // 	    int lo = 0;
+    // 	    int hi = data.length;
+    // 	    int mi = lo/2 + hi/2;
+    // 	    int[] temp = data.length/2;
 	
-    }
+    // }
 
-    public static void merge(int[] data, int[] temp, int lo, int mi, int hi){
+    public static int[]  merge(int[] data, int[] temp){//, int lo, int mi, int hi){
 	int[] total = new int[data.length+temp.length];
 	int di = 0;
 	int ti = 0;
-	while (di < data.length && ti < data.length){
-	    
+	int index = 0;
+	while (di < data.length || ti < data.length){
+	    if (data[di] <= temp[ti]){
+		total[index] = data[di];
+		di++;
+	    } else {
+		total[index] = temp[ti];
+		ti++;
+	    }
+	    index++;
 	}
+	//data = total;
+	return total;
     }
-    public static void  msort(int[] data, int[] temp, int lo, int hi){
+    public static String toString(int[] data){
+	String result = "";
+	int index = 0;
+	while (index < data.length){
+	    result += data[index];
+	    index++;
+	}
+	return result;
+    }
+    // public static void  msort(int[] data, int[] temp, int lo, int hi){
 	
+    // }
+    public static void main(String[] Arg){
+	int[] data = {1,2,3,4,5,6,7};
+	int[] test = {2,6,7,9,19};
+	System.out.println(toString(merge(data,test)));
+	System.out.println(toString(data));
     }
 }
