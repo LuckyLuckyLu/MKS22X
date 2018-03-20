322b077c3f1f0be7a7adc5dbcb227e59b49b89a2
LuckyLuckyLu <alu8@stuy.edu>
2018-03-20 15:45:48 -0400

    Worked on merge.

---
+++ 07eMERGEncy/Merge.java
@@ -0,0 +1,17 @@
+public class Merge{
+    public static void mergesort(int[] data){
+	
+    }
+
+    public static void merge(int[] data, int[] temp, int lo, int mi, int hi){
+	int[] total = new int[data.length+temp.length];
+	int di = 0;
+	int ti = 0;
+	while (di < data.length && ti < data.length){
+	    
+	}
+    }
+    public static void  msort(int[] data, int[] temp, int lo, int hi){
+	
+    }
+}
