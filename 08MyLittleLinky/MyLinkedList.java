c2719c28946fd2934f7da8637ad345353a179fed
LuckyLuckyLu <alu8@stuy.edu>
2018-04-08 15:50:54 -0400

    Working on MYLINKEDLIST.java

--- 08MyLittleLinky/MyLinkedList.java
+++ 08MyLittleLinky/MyLinkedList.java
@@ -5,37 +5,114 @@ public class MyLinkedList{
   //This method will help you write other
   //methods, it is private to protect your list
   private Node getNode(int index){
-    
+    if (index < 0 || index > size){
+      throw new IndexOutOfBoundsException();
+    }
+    if (index == 0){
+      return start;
+    }
+    if (index == size){
+      return end;
+    }
+    Node found = start;
+    for (int i = 0; i <= index; i++){
+      found = found.getNext();
+    }
+    return found;
   }
 
 
-  //public MyLinkedList();
-  //public String toString();
-  //public void clear();
+  public MyLinkedList(){
+    size = 0;
+    start = null;
+    end = null;
+  }
+  public String toString(){
+    String result = "";
+    for (int i = 0; i < size; i++){
+      result += getNode(i) + " ";
+    }
+    return result;
+  }
+  public void clear(){
+    start = null;
+    end = null;
+    size = 0;
+  }
   public int size(){
     return size;
   }
   
-  //public Integer get(int index);//exceptions!
-  //public Integer set(int index, Integer value);//exceptions!
+  public Integer get(int index){//exceptions!
+    if (index < 0 || index > size){
+      throw new IndexOutOfBoundsException();
+    }
+    return getNode(index).getValue();
+  }
+  
+  public Integer set(int index, Integer value){//exceptions!
+    if (index < 0 || index > size){
+      throw new IndexOutOfBoundsException();
+    }
+    getNode(index).setDat(value);
+    return value;
+  }
 
-  //public int indexOf(Integer value);
+  public int indexOf(Integer value){
+    Node found = start;
+    for (int i = 0; i < size; i++){
+      if ((found.getValue()) == (value)){
+        return i;
+      }
+      found = found.getNext();
+    }
+    return -1;
+  }
 
-  //public boolean add(Integer newData);
-  //public void add(int index, Integer value);//exceptions!
+  public boolean add(Integer newData){
+    add(size,newData);
+    return true;
+  }
+  public void add(int index, Integer value){//exceptions!
+    if (index < 0 || index > size){
+      throw new IndexOutOfBoundsException();
+    }
+    Node NEW = new Node(null,null,value);
+    if (size == 0){
+      start = NEW;
+      end = NEW;
+    }
+    if (index == 0){
+      start.setPre(NEW);
+      NEW.setNex(start);
+      start = NEW;
+    } else if (index == size) {
+      end.setNex(NEW);
+      NEW.setPre(end);
+      end = NEW;
+    } else {
+      Node After = getNode(index);
+      Node Before = After.getPrev();
+      Before.setNex(NEW);
+      After.setPre(NEW);
+      NEW.setPre(Before);
+      NEW.setNex(After);
+    }
+    size ++;
+  }
 
   //The remove methods can cause a problem, this is why we shouldn't 
   //use an int as the data, we need objects to distinguish between index and data
   //public boolean remove(Integer value);
   //public boolean remove(int index);//exceptions!
 
-  private class Nodes{
+   class Node{
     private Node next,prev;
     private int data;
 
-    public Nodes(int Next, int Prev, int Data){
-	    next = Next;
+     public Node(Node Prev, Node Next, int Data){
 	    prev = Prev;
+	    next = Next;
 	    data = Data;
     }
 
@@ -54,10 +131,10 @@ public class MyLinkedList{
     public void setDat(int Data){
 	    data = Data;
     }
-    public void setPre(int Prev){
+    public void setPre(Node Prev){
 	    prev = Prev;
     }
-    public void setNex(int Next){
+    public void setNex(Node Next){
 	    next = Next;
     }
 	
