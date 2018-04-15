644015685eec4ddcaf0fcd513c981f026694a7d5
LuckyLuckyLu <alu8@stuy.edu>
2018-04-15 01:22:48 -0400

    Working on RadixSort.

--- 08MyLittleLinky/MyLinkedListImproved.java
+++ 08MyLittleLinky/MyLinkedListImproved.java
@@ -1,275 +1,283 @@
 import java.util.*;
 public class MyLinkedListImproved<T extends Comparable<T>> implements Iterable<T>{
-    // class MyLinkedListImproved<T extends Comparable<T>>{
-    private Node start,end;
-    private int size;
+  // class MyLinkedListImproved<T extends Comparable<T>>{
+  private Node start,end;
+  private int size;
     
-    //This method will help you write other
-    //methods, it is private to protect your list
-    private Node getNode(int index){
-	if (index < 0 || index >= size){
+  //This method will help you write other
+  //methods, it is private to protect your list
+  private Node getNode(int index){
+    if (index < 0 || index >= size){
 	    throw new IndexOutOfBoundsException();
-	}
-	if (index == 0){
+    }
+    if (index == 0){
 	    return start;
-	}
-	if (index == size - 1){
+    }
+    if (index == size - 1){
 	    return end;
-	}
-	Node found = start;
-	for (int i = 0; i < index; i++){
+    }
+    Node found = start;
+    for (int i = 0; i < index; i++){
 	    found = found.getNext();
-	}
-	return found;
     }
+    return found;
+  }
 
 
-    public MyLinkedListImproved(){
-	size = 0;
-	start = null;
-	end = null;
-    }
+  public MyLinkedListImproved(){
+    size = 0;
+    start = null;
+    end = null;
+  }
     
-    public String toString(){
-	String result = "";
-	for (int i = 0; i < size; i++){
+  public String toString(){
+    String result = "";
+    for (int i = 0; i < size; i++){
 	    result += getNode(i) + " ";
-	}
-	return result;
     }
-    public void clear(){
-	start = null;
-	end = null;
-	size = 0;
-    }
-    public int size(){
-	return size;
-    }
+    return result;
+  }
+  public void clear(){
+    start = null;
+    end = null;
+    size = 0;
+  }
+  public int size(){
+    return size;
+  }
   
-    public T get(int index){//exceptions!
-	if (index < 0 || index >= size){
+  public T get(int index){//exceptions!
+    if (index < 0 || index >= size){
 	    throw new IndexOutOfBoundsException();
-	}
-	return getNode(index).getValue();
     }
+    return getNode(index).getValue();
+  }
   
-    public T set(int index, T value){//exceptions!
-	if (index < 0 || index >= size){
+  public T set(int index, T value){//exceptions!
+    if (index < 0 || index >= size){
 	    throw new IndexOutOfBoundsException();
-	}
-	getNode(index).setDat(value);
-	return value;
     }
+    getNode(index).setDat(value);
+    return value;
+  }
 
-    public int indexOf(T value){
-	Node found = start;
-	for (int i = 0; i < size; i++){
+  public int indexOf(T value){
+    Node found = start;
+    for (int i = 0; i < size; i++){
 	    if ((found.getValue()).equals(value)){
-		return i;
+        return i;
 	    }
 	    found = found.getNext();
-	}
-	return -1;
     }
+    return -1;
+  }
 
-    public boolean add(T newData){
-	add(size,newData);
-	return true;
-    }
-    public void add(int index, T value){//exceptions!
-	if (index < 0 || index > size){
+  public boolean add(T newData){
+    add(size,newData);
+    return true;
+  }
+  public void add(int index, T value){//exceptions!
+    if (index < 0 || index > size){
 	    throw new IndexOutOfBoundsException();
-	}
-	Node NEW = new Node(null,null,value);
-	if (size == 0){
+    }
+    Node NEW = new Node(null,null,value);
+    if (size == 0){
 	    start = NEW;
 	    end = NEW;
-	} else if (index == 0){
+    } else if (index == 0){
 	    start.setPre(NEW);
 	    NEW.setNex(start);
 	    start = NEW;
-	} else if (index == size) {
+    } else if (index == size) {
 	    end.setNex(NEW);
 	    NEW.setPre(end);
 	    end = NEW;
-	} else {
+    } else {
 	    Node After = getNode(index);
 	    Node Before = After.getPrev();
 	    Before.setNex(NEW);
 	    After.setPre(NEW);
 	    NEW.setPre(Before);
 	    NEW.setNex(After);
-	}
-	size ++;
     }
+    size ++;
+  }
 
-    //The remove methods can cause a problem, this is why we shouldn't 
-    //use an int as the data, we need objects to distinguish between index and data
-    public boolean remove(T value){
-	int index = indexOf(value);
-	if (index == -1){
+  //The remove methods can cause a problem, this is why we shouldn't 
+  //use an int as the data, we need objects to distinguish between index and data
+  public boolean remove(T value){
+    int index = indexOf(value);
+    if (index == -1){
 	    return false;
-	}
-	Node current = getNode(index);
-	if (index == 0){
+    }
+    Node current = getNode(index);
+    if (index == 0){
 	    Node next = current.getNext();
 	    next.setPre(null);
 	    start = next;
-	} else if (index == size-1){
+    } else if (index == size-1){
 	    Node previous = current.getPrev();
 	    previous.setNex(null);
 	    end = previous;
-	} else {
+    } else {
 	    Node previous = current.getPrev();
 	    Node next = current.getNext();
 	    previous.setNex(next);
 	    next.setPre(previous);
-	}
-	size--;
-	return true;
     }
-    public boolean remove(int index){//exceptions!
-	if (index < 0 || index >= size){
+    size--;
+    return true;
+  }
+  public boolean remove(int index){//exceptions!
+    if (index < 0 || index >= size){
 	    throw new IndexOutOfBoundsException();
-	}
-	Node current = getNode(index);
-	if (index == 0){
+    }
+    Node current = getNode(index);
+    if (index == 0){
 	    Node next = current.getNext();
 	    next.setPre(null);
 	    start = next;
-	} else if (index == size-1){
+    } else if (index == size-1){
 	    Node previous = current.getPrev();
 	    previous.setNex(null);
 	    end = previous;
-	} else {
+    } else {
 	    Node previous = current.getPrev();
 	    Node next = current.getNext();
 	    previous.setNex(next);
 	    next.setPre(previous);
-	}
-	size--;
-	return true;
     }
+    size--;
+    return true;
+  }
 
-    class Node{
-	private Node next,prev;
-	private T data;
+  class Node{
+    private Node next,prev;
+    private T data;
 
-	public Node(Node Prev, Node Next, T Data){
+    public Node(Node Prev, Node Next, T Data){
 	    prev = Prev;
 	    next = Next;
 	    data = Data;
-	}
+    }
 
-	public Node getNext(){
+    public Node getNext(){
 	    return next;
-	}
-	public Node getPrev(){
+    }
+    public Node getPrev(){
 	    return prev;
-	}
-	public T getValue(){
+    }
+    public T getValue(){
 	    return data;
-	}
-	public String toString(){
+    }
+    public String toString(){
 	    return "" + data;
-	}
-	public void setDat(T Data){
+    }
+    public void setDat(T Data){
 	    data = Data;
-	}
-	public void setPre(Node Prev){
+    }
+    public void setPre(Node Prev){
 	    prev = Prev;
-	}
-	public void setNex(Node Next){
+    }
+    public void setNex(Node Next){
 	    next = Next;
-	}
+    }
 	
 	
-	/* Node next,prev
-	   int data
+    /* Node next,prev
+       int data
 
-	   Node Methods:
-	   Node getNext()
-	   Node getPrev()
-	   int getValue()
-	   String toString
-	   (also all the set methods to go with them)
-	*/
+       Node Methods:
+       Node getNext()
+       Node getPrev()
+       int getValue()
+       String toString
+       (also all the set methods to go with them)
+    */
 	
-    }
-    public Iterator<T> iterator(){
-	return new ImprovIterator();
-    }
-    private class ImprovIterator implements Iterator<T>{
-	public Node current;
+  }
+  public Iterator<T> iterator(){
+    return new ImprovIterator();
+  }
+  private class ImprovIterator implements Iterator<T>{
+    public Node current;
 	
-	public ImprovIterator(){
+    public ImprovIterator(){
 	    current = start;
-	}
-	public boolean hasNext(){
+    }
+    public boolean hasNext(){
 
 	    if (current.getNext().getValue().equals(null)){
-		return false;
+        return false;
 	    } else {
-		return true;
+        return true;
 	    }
-	}
-	public T next(){
+    }
+    public T next(){
 	    T value = current.getValue();
 	    current = current.getNext();
 	    return value;
-	}
     }
+  }
 
-    public int min(){
-	T minTy = null;
-	int index = 0;
-	if (start.getValue() != null){
+  public int min(){
+    T minTy = null;
+    int index = 0;
+    if (start.getValue() != null){
 	    minTy = start.getValue();
-	} else {
+    } else {
 	    return -1;
-	}
-	for (T x : this ){
+    }
+    for (T x : this ){
 	    if (minTy.compareTo(x) > 0){
-		minTy = x;
-		index++;
+        minTy = x;
+        index++;
 	    }
-	}
-	return index;
     }
-    public int max(){
-	T maxTy = null;
-	int index = 0;
-	if (start.getValue() != null){
+    return index;
+  }
+  public int max(){
+    T maxTy = null;
+    int index = 0;
+    if (start.getValue() != null){
 	    maxTy = start.getValue();
-	} else {
+    } else {
 	    return -1;
-	}
-	for (T x : this ){
+    }
+    for (T x : this ){
 	    if (maxTy.compareTo(x) < 0){
-		maxTy = x;
-		index++;
+        maxTy = x;
+        index++;
 	    }
-	}
-	return index;
     }
-    public void extend(MyLinkedListImproved<T> data){
-	if (data.size == 0){
+    return index;
+  }
+  public void extend(MyLinkedListImproved<T> data){
+    if (data.size == 0){
 	    return;
-	}
-	this.getNode(size()-1).setNex(data.getNode(0));
-	this.end = data.end;
-	data.clear();
     }
-    public class Sorts{
-	public static void radixsort(MyLinkedListImproved<T> data){
-	private MyLinkedListImproved<T> List;
-	private @suppresswarnings("unchecked") MyLinkedListImproved<T>[] data = new MyLinkedListImproved<>[10];
-	private int max = data.getNode(data.max());
-	for(int i = 1; i < max; i*10){
-	    
-	}
+    this.getNode(size()-1).setNex(data.getNode(0));
+    this.end = data.end;
+    data.clear();
+  }
+  
+  public class Sorts{
+    public void radixsort(MyLinkedListImproved<Integer> data){
+      if (data.size == 0){
+        return;
+      }
+      @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] info = new MyLinkedListImproved[10];
+      //int max = info.get(data.max());
+      int maxValue = data.get(data.max());
+      for(int i = 1; i < maxValue; i= i*10){
+        System.out.println(maxValue/(10*i));
+      }
 	
 	
-	}
     }
+  }
+  public static void main(String[] args){
+    MyLinkedListImproved<Integer> x = new MyLinkedListImproved<>();
+    MyLinkedListImproved<Integer> y = new MyLinkedListImproved<>();
+  }
 }
