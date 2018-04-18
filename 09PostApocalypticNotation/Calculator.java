b42ee37e8d7a23868b8dc7f92974678c28bd15b4
LuckyLuckyLu <alu8@stuy.edu>
2018-04-18 14:40:28 -0400

    Created 10MyDQ, fixed eval.java to use LinkedList as stack rather than queue, and renamed eval.java as Calculator.java

---
+++ 09PostApocalypticNotation/Calculator.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+public class Calculator{
+  public static Double eval(String Expression){
+    LinkedList<Double> stack = new LinkedList<Double>();
+    String[] data = Expression.split(" ");
+    for (int i = 0; i < data.length; i++){
+      if (data[i].equals("+")){
+        stack.push(stack.pop() + stack.pop());
+      } else if (data[i].equals("-")){
+        //System.out.println(stack.pop() + "-" + stack.pop());
+	  stack.push(-1 * (stack.pop() - stack.pop()));
+      } else if (data[i].equals("*")){
+        stack.push(stack.pop() * stack.pop());
+      } else if (data[i].equals("/")){
+        stack.push(1.0 / stack.pop() * stack.pop());
+      } else if (data[i].equals("%")){
+	  Double divisor = stack.pop();
+	  stack.push(stack.pop() % divisor);
+      } else {
+        stack.push(Double.parseDouble(data[i]));
+        //System.out.println(Double.parseDouble(data[i]));
+      }
+    }
+    //return 0.0;
+    return stack.pop();
+  }
+  public static void main(String[] args){
+    System.out.println(eval("1 2 %"));
+  }
+}
