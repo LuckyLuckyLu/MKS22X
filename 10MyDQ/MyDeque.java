0719c38c6e011cd21d9271f73b88c053a57e28c2
LuckyLuckyLu <alu8@stuy.edu>
2018-04-19 00:02:03 -0400

    Working on MyDQ.java

--- 10MyDQ/MyDeque.java
+++ 10MyDQ/MyDeque.java
@@ -1,18 +1,58 @@
 public class MyDeque{
-    private int size;
-    private Integer[] data;
-    public MyDeque(){
-	size = 10;
-	data = new Integer[10];
+  private int size, start, end;
+  private Integer[] data;
+  public MyDeque(){
+    size = 10;
+    data = new Integer[10];
+    start = 5;
+    end = 0;
+  }
+  public MyDeque(int Size){
+    if (Size <= 0){
+      throw new IllegalArgumentException();
     }
-    public MyDeque(int Size){
-	size = Size;
-	data = new Integer[Size];	
+    size = Size;
+    data = new Integer[Size];
+    start = Size / 2;
+    end = 0;
+  }
+  public void reSize(int multiple){
+    int newSize = size * multiple;
+    Integer[] newSized = new Integer[size];
+    for (int i = 0; i < size; i++){
+      newSized[i] = data[(i+start)%size];
     }
-    public reSize(int multiple){
-	size *= multiple;
-	Integer[] newSized = new Integer[size];
-	for (int i = 0;){
-	}
+    size = newSize;
+    data = newSized;
+  }
+  public void addFirst(Integer value){
+    if (value == null){
+      throw new NullPointerException();
     }
+    if (data[start-1] == null){
+      data[start-1] = value;
+      start -= 1;
+      size+=
+    } else {
+      reSize(2);
+      addFirst(value);
+    }
+    
+  }
+  public void addLast(Integer value){
+    if (value == null){
+      throw new NullPointerException();
+    }
+    if (data[end+1] == null){
+      data[end-1] = value;
+      end += 1;
+    } else {
+      reSize(2);
+      addLast(value);
+    }
+  }
+  public Integer removeFirst(){
+    if (size)
+  }
+  
 }
