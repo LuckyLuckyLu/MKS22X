4453adfe56e3b094656e6bee9f198a687de50be9
LuckyLuckyLu <alu8@stuy.edu>
2018-05-09 06:32:58 -0400

    Working all day and night on MazeSolver.

--- 14RunningAroundInA_MAZE_MENT/FrontierQueue.java
+++ 14RunningAroundInA_MAZE_MENT/FrontierQueue.java
@@ -1,12 +1,14 @@
 import java.util.LinkedList;
 public class FrontierQueue implements Frontier{
-    private LinkedList<Location> list; 
-    public FrontierQueue(){
-	list = new LinkedList<>();
-    }
-    public Location next(){
-	return null;
-    }
-    public void add(Loc
+  private LinkedList<Location> list; 
+  public FrontierQueue(){
+    list = new LinkedList<>();
+  }
+  public Location next(){
+    return null;
+  }
+  public void add(Location Thingy){
+    list.add(Thingy);
+  }
     
 }
