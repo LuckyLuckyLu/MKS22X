6e9db8ec37da4d47ad26e8e71f3c3648835c0398
LuckyLuckyLu <alu8@stuy.edu>
2018-05-15 16:55:40 -0400

    Adding A* to MazeSolver.java

--- 14RunningAroundInA_MAZE_MENT/Location.java
+++ 14RunningAroundInA_MAZE_MENT/Location.java
@@ -1,39 +1,49 @@
 public class Location implements Comparable<Location>{
-  private int x,y,distance;
-  private Location previous;
+    private int x,y,distance,SoFar;
+    private Location previous;
 
-  public Location(int _x, int _y, Location prev){
-    x = _x;
-    y = _y;
-    previous = prev;
-  }
-  public Location(int _x, int _y, Location prev, int dist){
-    x = _x;
-    y = _y;
-    previous = prev;
-    distance = dist;
-  }
-
-  public Location getPrevious(){
-    return previous;
-  }
-  public int getX(){
-    return x;
-  }
-  public int getY(){
-    return y;
-  }
-  public int getDistance(){
-    return distance;
-  }
-  public int compareTo(Location other){
-    if(getDistance() < other.getDistance()){
-      return -1;
-    } else {
-      return 1;
+    public Location(int _x, int _y, Location prev){
+	x = _x;
+	y = _y;
+	previous = prev;
     }
-  }
-  public String toString(){
-    return "X value: " + getX() + " |Y value: " + getY() + " |DistanceToE: " + getDistance();
-  }
+    public Location(int _x, int _y, Location prev, int dist){
+	x = _x;
+	y = _y;
+	previous = prev;
+	distance = dist;
+    }
+    public Location(int _x, int _y, Location prev, int dist, int SoClose){
+	x = _x;
+	y = _y;
+	previous = prev;
+	distance = dist;
+	SoFar = SoClose;
+    }
+
+    public Location getPrevious(){
+	return previous;
+    }
+    public int getX(){
+	return x;
+    }
+    public int getY(){
+	return y;
+    }
+    public int getDistance(){
+	return distance;
+    }
+    public int getSoFar(){
+	return SoFar;
+    }
+    public int compareTo(Location other){
+	if(getDistance() < other.getDistance()){
+	    return -1;
+	} else {
+	    return 1;
+	}
+    }
+    public String toString(){
+	return "X value: " + getX() + " |Y value: " + getY() + " |DistanceToE: " + getDistance();
+    }
 }
--- 14RunningAroundInA_MAZE_MENT/MazeSolver.java
+++ 14RunningAroundInA_MAZE_MENT/MazeSolver.java
@@ -3,86 +3,92 @@ import java.io.File;
 import java.util.Scanner;
 
 public class MazeSolver{
-  private Maze maze;
-  private Frontier frontier;
+    private Maze maze;
+    private Frontier frontier;
+    private boolean StarPower = false;
+    
 
-  public MazeSolver(String fileName){
-    maze = new Maze(fileName);
-    //System.out.println(maze);
+    public MazeSolver(String fileName){
+	maze = new Maze(fileName);
+	//System.out.println(maze);
 
-  }
-  //Default to BFS
-  public boolean solve(){
-    return solve(0);
-  }
+    }
+    //Default to BFS
+    public boolean solve(){
+	return solve(0);
+    }
 
-  //mode: required to allow for alternate solve modes.
-  //0: BFS
-  //1: DFS
-  public boolean solve(int mode){
-    if (mode == 0){
+    public void AstroBoy(){
+	StarPower = !StarPower;
+    }
+
+    //mode: required to allow for alternate solve modes.
+    //0: BFS
+    //1: DFS
+    public boolean solve(int mode){
+	if (mode == 0){
 	    frontier = new FrontierQueue();		
     
-    } else if (mode == 1){
-      frontier = new FrontierStack();
-    } else {
-      frontier = new FrontierPriorityQueue();
-    }
+	} else if (mode == 1){
+	    frontier = new FrontierStack();
+	} else {
+	    frontier = new FrontierPriorityQueue();
+	}
 
-    frontier.add(maze.getStart());
-    Location end = maze.getEnd();
+	frontier.add(maze.getStart());
+	Location end = maze.getEnd();
 
-    while (frontier.hasNext()){
-      Location Spot = frontier.next();
-      Location[] battleFrontier = maze.getNeighbors(Spot);
-      for (int i = 0; i < 4; i++){
-        Location nextSpot = battleFrontier[i];
-        if (nextSpot != null){
-          int nextX = nextSpot.getX();
-          int nextY = nextSpot.getY();
-          char nextValue = maze.get(nextX,nextY);
-          if (nextValue == ' '){
-            frontier.add(nextSpot);
-          }
-          if (nextValue == 'E'){
-            maze.set(Spot.getX(),Spot.getY(),'.');
-            maze.end = new Location(nextX,nextY,Spot);
-            end = maze.getEnd();
-            while (end.getPrevious() != null && !(end.getPrevious().equals(maze.getStart()))){
-              end = end.getPrevious();
-              maze.set(end.getX(),end.getY(),'@');
-            }
-            //System.out.println(maze.toStringColor());
-            return true;
-          }
-        }
-      }
-      //System.out.println(maze.toStringColor());
-      if (maze.get(Spot.getX(),Spot.getY()) != 'S'){
-        maze.set(Spot.getX(),Spot.getY(),'.');
-      }
+	while (frontier.hasNext()){
+	    Location Spot = frontier.next();
+	    Location[] battleFrontier = maze.getNeighbors(Spot);
+	    for (int i = 0; i < 4; i++){
+		Location nextSpot = battleFrontier[i];
+		if (nextSpot != null){
+		    int nextX = nextSpot.getX();
+		    int nextY = nextSpot.getY();
+		    char nextValue = maze.get(nextX,nextY);
+		    if (nextValue == ' '){
+			frontier.add(nextSpot);
+		    }
+		    if (nextValue == 'E'){
+			maze.set(Spot.getX(),Spot.getY(),'.');
+			maze.end = new Location(nextX,nextY,Spot);
+			end = maze.getEnd();
+			while (end.getPrevious() != null && !(end.getPrevious().equals(maze.getStart()))){
+			    end = end.getPrevious();
+			    maze.set(end.getX(),end.getY(),'@');
+			}
+			//System.out.println(maze.toStringColor());
+			return true;
+		    }
+		}
+	    }
+	    //System.out.println(maze.toStringColor());
+	    if (maze.get(Spot.getX(),Spot.getY()) != 'S'){
+		maze.set(Spot.getX(),Spot.getY(),'.');
+	    }
 
-    }
+	}
 	
-    //initialize your frontier
-    //while there is stuff in the frontier:
-    //  get the next location
-    //  process the location to find the locations (use the maze to do this)
-    //  check if any locations are the end, if you found the end just return true!
-    //  add all the locations to the frontier
-    //when there are no more values in the frontier return false
-    return false;
-  }
+	//initialize your frontier
+	//while there is stuff in the frontier:
+	//  get the next location
+	//  process the location to find the locations (use the maze to do this)
+	//  check if any locations are the end, if you found the end just return true!
+	//  add all the locations to the frontier
+	//when there are no more values in the frontier return false
+	return false;
+    }
 
-  public String toString(){
-    return maze.toString();
-  }
+    public String toString(){
+	return maze.toString();
+    }
 
-  public static void main(String[] args){
-    MazeSolver x = new MazeSolver("data3.dat");
-    System.out.println(x.solve(2));
-    System.out.println(x);
+    public static void main(String[] args){
+	MazeSolver x = new MazeSolver("data3.dat");
+	System.out.println(x.solve(2));
+	System.out.println(x);
 
 
-  }
+    }
 }
